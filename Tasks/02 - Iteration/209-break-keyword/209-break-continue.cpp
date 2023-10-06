edb082fa91b47e66bcdfee7ade72cfd01dde0a14
darronn <fin.dyer@outlook.com>
2023-10-06 14:14:53 +0100

    eyes

--- Tasks/02 - Iteration/209-break-keyword/209-break-continue.cpp
+++ Tasks/02 - Iteration/209-break-keyword/209-break-continue.cpp
@@ -1,7 +1,8 @@
-#include <stdio.h>
+#include <stdio.h> 
 
 int main()
 {
+    bool y = false;
     
     for (int i = 0; i <= 10; i++)   // potentially loop from 1 to 10  
     {
@@ -10,12 +11,13 @@ int main()
         if (c == 'q')          
         {
             printf("\tUser stopped the loop early at %d\n", i);
-            break;                  // break out of the for loop
-            char y = y;
-            if (y = y)
+            y = true;
+            if (y == true);
             {
-                printf("you exited the loop Loop counter = %d\n", i);
+                printf("you exited the loop, Loop counter = %d\n", i);
             }
+            break;                  // break out of the for loop
+             
         }
         
         
--- Tasks/02 - Iteration/211-nested-loops/211-nested-loops.cpp
+++ Tasks/02 - Iteration/211-nested-loops/211-nested-loops.cpp
@@ -11,9 +11,13 @@ int main()
         for (int j = 1; j <= 5; j++)    // Inner loop executes 5 times for each outer loop run so 10 time in total
         {
             printf("\tInner loop number %d\n", j);  // note the use of the tab (\t) option at the start to improve the layout
-            
+            puts("        i,j,k");
             //Task - Write another level of nesting here
-
+            for (int k = 1; k <= 3; k++)
+            {
+                
+                printf("\t%d,%d,%d\n",i,j,k );
+            }
         }
     }
 }
